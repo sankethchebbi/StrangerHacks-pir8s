ed8b480e2bf9bdcd7acb402da5dc848b5a439909
sanketh <sankethchebbi@gmail.com>
2023-03-31 18:39:44 +0530

    genesis

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './NavigationBar';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <BasicExample/>
+    </div>
+  );
+}
+
+export default App;
